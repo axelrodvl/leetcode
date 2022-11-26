692abc56d593b89cdef49ece19061d2afedd3e2e
Vadim Axelrod <vadim@axelrod.co>
2022-11-26 19:35:34 +0400

    reverse-linked-list

---
+++ src/main/java/co/axelrod/leetcode/list/LinkedListUtil.java
@@ -0,0 +1,48 @@
+package co.axelrod.leetcode.list;
+
+public class LinkedListUtil {
+    private LinkedListUtil() {
+        // Utility class
+    }
+
+    public static boolean equalLinkedList(ListNode firstListNode, ListNode secondListNode) {
+        if (firstListNode == null && secondListNode == null) {
+            return true;
+        }
+
+        while (firstListNode != null && secondListNode != null) {
+            if (firstListNode.val != secondListNode.val) {
+                return false;
+            }
+            if (firstListNode.next == null && secondListNode.next == null) {
+                return true;
+            }
+            if (firstListNode.next == null) {
+                return false;
+            }
+            if (secondListNode.next == null) {
+                return false;
+            }
+            firstListNode = firstListNode.next;
+            secondListNode = secondListNode.next;
+        }
+
+        return true;
+    }
+
+    public static ListNode createLinkedList(Integer args[]) {
+        if (args.length == 0) {
+            return null;
+        }
+
+        ListNode first = new ListNode(args[0]);
+        ListNode current = first;
+        int index = 1;
+        while (index < args.length) {
+            current.next = new ListNode(args[index++]);
+            current = current.next;
+        }
+
+        return first;
+    }
+}
---
+++ src/test/java/co/axelrod/leetcode/ReverseLinkedList.java
@@ -0,0 +1,59 @@
+package co.axelrod.leetcode;
+
+import co.axelrod.leetcode.list.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static co.axelrod.leetcode.list.LinkedListUtil.createLinkedList;
+import static co.axelrod.leetcode.list.LinkedListUtil.equalLinkedList;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+class ReverseLinkedList {
+    @Test
+    void test() {
+        ListNode input = createLinkedList(new Integer[]{});
+        ListNode expectedOutput = createLinkedList(new Integer[]{});
+        ListNode output = reverseList(input);
+        assertTrue(equalLinkedList(expectedOutput, output));
+
+        input = createLinkedList(new Integer[]{1});
+        expectedOutput = createLinkedList(new Integer[]{1});
+        output = reverseList(input);
+        assertTrue(equalLinkedList(expectedOutput, output));
+
+        input = createLinkedList(new Integer[]{1, 2});
+        expectedOutput = createLinkedList(new Integer[]{2, 1});
+        output = reverseList(input);
+        assertTrue(equalLinkedList(expectedOutput, output));
+
+        input = createLinkedList(new Integer[]{1, 2, 3});
+        expectedOutput = createLinkedList(new Integer[]{3, 2, 1});
+        output = reverseList(input);
+        assertTrue(equalLinkedList(expectedOutput, output));
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode next = head.next;
+
+        while (next != null) {
+            ListNode nextNext = next.next;
+
+            current.next = previous;
+            next.next = current;
+
+            previous = current;
+            current = next;
+            next = nextNext;
+        }
+
+        return current;
+    }
+}
