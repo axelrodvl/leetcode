f926a8868a7d6286b53a9cba85cc1ae67ac53551
Vadim Axelrod <vadim@axelrod.co>
2022-11-27 16:47:51 +0400

    contains-duplicate

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 - [20. Valid Parentheses](https://leetcode.com/problems/valid-parentheses/)
 - [121. Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
 - [206. Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/)
+- [217. Contains Duplicate](https://leetcode.com/problems/contains-duplicate/)
 - [1207. Unique Number of Occurrences](https://leetcode.com/problems/unique-number-of-occurrences/)
---
+++ src/test/java/co/axelrod/leetcode/ContainsDuplicate.java
@@ -0,0 +1,33 @@
+package co.axelrod.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * https://leetcode.com/problems/contains-duplicate/
+ */
+public class ContainsDuplicate {
+    @Test
+    void containsDuplicateTest() {
+        assertTrue(containsDuplicate(new int[]{1, 2, 3, 1}));
+        assertFalse(containsDuplicate(new int[]{1, 2, 3, 4}));
+        assertTrue(containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> unique = new HashSet<>();
+        for(int number : nums) {
+            if (unique.contains(number)) {
+                return true;
+            } else {
+                unique.add(number);
+            }
+        }
+        return false;
+    }
+}
