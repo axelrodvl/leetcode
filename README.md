5a4b1df6e77b350b054ba8b8106fb9ff28315122
Vadim Axelrod <vadim@axelrod.co>
2022-11-27 09:48:33 +0400

    palindrome-number

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # LeetCode
 
 `src/test/java/co/axelrod/leetcode`:
+- [9. Palindrome Number](https://leetcode.com/problems/palindrome-number/)
 - [13. Roman to Integer](https://leetcode.com/problems/roman-to-integer/)
 - [20. Valid Parentheses](https://leetcode.com/problems/valid-parentheses/)
 - [121. Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
---
+++ src/test/java/co/axelrod/leetcode/PalindromeNumber.java
@@ -0,0 +1,40 @@
+package co.axelrod.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * https://leetcode.com/problems/palindrome-number/
+ */
+public class PalindromeNumber {
+    @Test
+    void isPalindromeTest() {
+        assertTrue(isPalindrome(1));
+        assertTrue(isPalindrome(11));
+        assertTrue(isPalindrome(121));
+        assertTrue(isPalindrome(100010001));
+        assertFalse(isPalindrome(-1));
+        assertFalse(isPalindrome(122));
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        if (x < 10) {
+            return true;
+        }
+
+        int number = x;
+        int revertedNumber = 0;
+        while (number > 0) {
+            revertedNumber = revertedNumber * 10 + number % 10;
+            number /= 10;
+        }
+
+        return x == revertedNumber;
+    }
+}
